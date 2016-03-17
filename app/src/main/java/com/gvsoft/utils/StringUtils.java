7ccc4dd073317dc4fea93ccd64b9da5d2958f7f3
unknown <>
2016-03-17 16:17:41 +0800

    Initial commit

---
+++ app/src/main/java/com/gvsoft/utils/StringUtils.java
@@ -0,0 +1,19 @@
+package com.gvsoft.utils;
+
+/**
+ * Created by ÀÔπ„÷« on 2016/3/15.
+ */
+public class StringUtils {
+
+    public static String getHello(){
+        return "Hello!";
+    }
+
+    public static String sayHello(String name){
+        return "Hello " + name + "!";
+    }
+
+    public static String sayByeBye(String name){
+        return "Bye, " + name;
+    }
+}
